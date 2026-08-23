47228dc859e419f9d748ae60117a45bcbfa10ba5
Emerson Oliveira <emerson.520@outlook.com>
2026-08-22 22:02:13 -0300

    Add: primeiro exercicio

---
+++ Modulo-1-Estrutura-sequencial/6.ExercioDeFixacao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Exercicio;
+
+class Program 
+{ 
+	static void Main(string[] args)
+	{ 
+		Console.Clear();
+
+		Console.Write("Entre com seu nome completo: ");
+		string nomeCompleto = Console.ReadLine();
+		Console.Write("Quantos quartos tem na sua casa? ");
+		int quartos = int.Parse(Console.ReadLine());
+		Console.Write("Entre com o preço de um produto qualquer: ");
+		double preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+		Console.Write("Entre com seu ultimo nome, sua idade e sua altura escreva na mesma lina com um espaço: ");
+
+		string[] dados = Console.ReadLine().Split(' ');
+		string ultimoNome = dados[0];
+		int idade = int.Parse(dados[1]);
+		float altura = float.Parse(dados[2]);
+
+		Console.WriteLine(nomeCompleto);
+		Console.WriteLine(quartos);
+		Console.WriteLine(preco.ToString("F2",CultureInfo.InvariantCulture));
+		Console.WriteLine(ultimoNome);
+		Console.WriteLine(idade);
+		Console.WriteLine(altura);
+	}
+}
