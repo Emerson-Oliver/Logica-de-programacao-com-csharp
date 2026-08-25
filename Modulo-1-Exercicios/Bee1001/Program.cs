e08d90abfa71747bf73bf98570566f2e2ac1de1c
Emerson Oliveira <emerson.520@outlook.com>
2026-08-25 16:15:50 -0300

    Iniciando Exercicios

---
+++ Modulo-1-Exercicios/Bee1001/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Bee1001;  
+
+class Program 
+{  
+	static void Main(string[] args) 
+	{  
+		int A = int.Parse(Console.ReadLine()?? "0");
+		int B = int.Parse(Console.ReadLine()?? "0");
+
+		int X = A + B;
+
+		Console.WriteLine("X = " + X);
+	}
+}
---
+++ Modulo-1-Exercicios/Bee1002/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Bee1002;  
+
+class Program 
+{  
+	static void Main(string[] args) 
+	{  
+			Console.Clear();
+
+			double raio = double.Parse(Console.ReadLine()?? "0" ,CultureInfo.InvariantCulture);
+
+			double n = 3.14159;
+
+			double area = n * Math.Pow(raio, 2); 
+
+			Console.WriteLine($"A={area.ToString("F4",CultureInfo.InvariantCulture)}");
+	}
+}
