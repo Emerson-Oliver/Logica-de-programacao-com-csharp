782b2e7ae32314bed62f5dfdd63a0177401617cf
Emerson Oliveira <emerson.520@outlook.com>
2026-08-25 15:52:23 -0300

    Finalizando explicações

---
+++ Modulo-1-Estrutura-sequencial/8.CalculadoraAreaTerreno/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace CalculadoraAreaTerreno;
+
+class Program
+{
+	static void Main(String[] args)
+	{ 
+		Console.Clear();
+		double largura = double.Parse(Console.ReadLine() ?? "0" ,CultureInfo.InvariantCulture);
+		double comprimento = double.Parse(Console.ReadLine()?? "0" ,CultureInfo.InvariantCulture);
+		double preco = double.Parse(Console.ReadLine()?? "0" ,CultureInfo.InvariantCulture);
+
+		double area = largura * comprimento;
+		double precoFinal = area * preco;
+
+		Console.WriteLine($"AREA = {area.ToString("F2",CultureInfo.InvariantCulture)}");
+		Console.WriteLine($"PRECO = R$ {precoFinal.ToString("F2",CultureInfo.InvariantCulture)}");
+	} 
+}
