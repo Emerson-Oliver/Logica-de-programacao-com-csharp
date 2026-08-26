e6b88af901cfb80f9d7966879f6bb7406065eb45
Emerson Oliveira <emerson.520@outlook.com>
2026-08-26 16:21:08 -0300

    Add: Exercicio Bee1003

---
+++ Modulo-1-Exercicios/Bee1003/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Bee1003;  
+
+class Program
+{ 
+	static void Main(string[] args) 
+	{  
+		Console.Clear();
+		int A = int.Parse(Console.ReadLine()?? "0");
+		int B = int.Parse(Console.ReadLine()?? "0");
+
+		int SOMA = A + B;
+
+		Console.WriteLine($"SOMA = {SOMA}");
+	}
+}
