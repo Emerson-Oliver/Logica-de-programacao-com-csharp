e3bcea19fa88622bb56fc1973f76c17d4b65b2d0
Emerson Oliveira <emerson.520@outlook.com>
2026-08-26 22:53:23 -0300

    Add: exercicios 1004 e 1005

---
+++ Modulo-1-Exercicios/Bee1004/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Bee1004;
+
+class Program  
+{  
+	static void Main(string[] args) 
+	{ 
+		Console.Clear();
+		int A = int.Parse(Console.ReadLine()?? "0");
+		int B = int.Parse(Console.ReadLine()?? "0");
+
+		int PROD = A * B;
+
+		Console.WriteLine($"PROD = {PROD}");
+	}
+}
---
+++ Modulo-1-Exercicios/Bee1005/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+namespace Bee1005;
+
+class Program  
+{  
+	static void Main(string[] args) 
+	{ 
+		Console.Clear();
+		double A = double.Parse(Console.ReadLine()?? "0", CultureInfo.InvariantCulture);
+		double B = double.Parse(Console.ReadLine()?? "0", CultureInfo.InvariantCulture);
+
+		double MEDIA = (A * 3.5 + B * 7.5) / 11.0;
+
+		Console.WriteLine($"MEDIA = {MEDIA.ToString("F5",CultureInfo.InvariantCulture)}");
+	}
+}
