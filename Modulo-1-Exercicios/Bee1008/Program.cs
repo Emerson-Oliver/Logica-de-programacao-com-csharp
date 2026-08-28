ef7a3d82dad129b2a18b217f04833bc459ba8f54
Emerson Oliveira <emerson.520@outlook.com>
2026-08-28 16:02:06 -0300

    Add: Exercicios 1008 e 1010

---
+++ Modulo-1-Exercicios/Bee1008/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Bee1008;
+
+class Program
+{
+	static void Main(string[] args)
+	{ 
+		Console.Clear();
+		int numeroDoFuncionario = int.Parse(Console.ReadLine()?? "0");
+		int numeroDeHorasTrabalhadas = int.Parse(Console.ReadLine()?? "0");
+		decimal valorPorHora = decimal.Parse(Console.ReadLine()?? "0" ,CultureInfo.InvariantCulture);
+
+		decimal salarioLiquido =  numeroDeHorasTrabalhadas * valorPorHora;
+
+		Console.WriteLine($"Number = {numeroDoFuncionario}");
+		Console.WriteLine($"SALARY U$ = {salarioLiquido.ToString("F2",CultureInfo.InvariantCulture)}");
+	}
+}
---
+++ Modulo-1-Exercicios/Bee1010/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Bee1010;  
+
+class Program
+{
+	static void Main(string[] args)
+	{ 
+		Console.Clear();
+		string[] dados = Console.ReadLine().Split(' ');
+		int codigoPeca1 = int.Parse(dados[0]);
+		int quantidadePeca1 = int.Parse(dados[1]);
+		double valorPeca1 = double.Parse(dados[2], CultureInfo.InvariantCulture);
+
+		dados = Console.ReadLine().Split(' ');		
+		int codigoPeca2 = int.Parse(dados[0]);
+		int quantidadePeca2 = int.Parse(dados[1]);
+		double valorPeca2 = double.Parse(dados[2], CultureInfo.InvariantCulture);
+
+		double valorAPagar = ((quantidadePeca1 * valorPeca1) + (quantidadePeca2 * valorPeca2));
+
+		Console.WriteLine($"Valor a pagar: R$ {valorAPagar.ToString("F2", CultureInfo.InvariantCulture)}");
+	}
+}
