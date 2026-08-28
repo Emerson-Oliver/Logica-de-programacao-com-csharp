8ead46979b864c01b11fb9d0233c28090cea9b24
Emerson Oliveira <emerson.520@outlook.com>
2026-08-27 23:10:21 -0300

    Add: Exercicio Bee1007

---
+++ Modulo-1-Exercicios/Bee1007/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Bee1007;  
+
+class Program
+{ 
+	
+	
+		static void Main(string[] args) 
+		{ 
+			Console.Clear();
+			int A = int.Parse(Console.ReadLine()?? "0");
+			int B = int.Parse(Console.ReadLine()?? "0");
+			int C = int.Parse(Console.ReadLine()?? "0");
+			int D = int.Parse(Console.ReadLine()?? "0");
+
+			int DIFERENCA = (A * B - C * D);
+
+			Console.WriteLine($"DIFERENCA = {DIFERENCA}");
+		}
+	
+}
